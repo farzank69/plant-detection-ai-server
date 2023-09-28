1c8268e6ab491083217ac58cd595907e3e6d12af
Raju Kumar <rajukumar@Rajus-MacBook-Air.local>
2023-09-28 08:53:32 +0530

    server image management

--- server.js
+++ server.js
@@ -1,11 +1,6 @@
 const express = require("express");
 const cors = require('cors');
-
-const classifyimg = require("./controllers/classifyImage");
-const loadmodel = require("./controllers/loadModel");
-const runclassifier = require("./controllers/runClassifier");
-const image = require("./controllers/detect");
-// const exp = require("constants");
+const path = require('path'); // Import the path module
 
 const app = express();
 const multer = require('multer');
@@ -15,27 +10,31 @@ app.use(express.json());
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, 'uploads/');
+      cb(null, './uploads/'); // Specify the directory where files will be stored (create the 'uploads' directory)
     },
     filename: (req, file, cb) => {
-      cb(null, Date.now() + '-' + file.originalname);
+      cb(null, Date.now() + path.extname(file.originalname)); // Generate a unique filename
     },
-  });
+});
 
+const upload = multer({ storage });
 
-  const upload = multer({ storage });
-// app.post("/", (req, res) => { runclassifier.runClassifier(req, res) });
+// Serve static files (if needed)
+app.use(express.static('public'));
 
+// Handle file upload
 app.post('/upload', upload.single('file'), (req, res) => {
     if (!req.file) {
-      return res.status(400).json({ error: 'No file uploaded' });
+        return res.status(400).json({ error: 'No file uploaded.' });
     }
-    res.json({ message: 'File uploaded successfully', filename: req.file.filename });
-  });
 
-app.post("/detect", (req,res) => { image.handleApiCall(req, res) })
+    // File was uploaded successfully
+    res.json({ message: 'File uploaded successfully.' });
+});
+
+app.post("/detect", (req, res) => { image.handleApiCall(req, res) })
 
 // runclassifier.runClassifier();
 app.listen(3000, () => {
     console.log("app is on port 3000");
-})
+});
